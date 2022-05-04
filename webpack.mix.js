d670dab5d312ed0e544920bfdcae1f349e72a5cd
Alex Brindley <alex@dodgyco.de>
2022-05-04 11:24:57 -0400

    Updated mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,8 @@
-let mix = require('laravel-mix');
-let fs = require('fs');
+let mix = require('laravel-mix')
+let fs = require('fs')
 
 // Mix Public Path
-mix.setPublicPath('web');
-
-// Mix Options
-mix.options({
-    clearConsole: Mix.isWatching()
-});
+mix.setPublicPath('web')
 
 // Extra Webpack Config
 mix.webpackConfig({
@@ -17,44 +12,24 @@ mix.webpackConfig({
                 test: /\.scss$/,
                 loader: 'import-glob-loader',
             },
-
-            // Global jquery
-            /* {
-                test: require.resolve('jquery'),
-                use: [{
-                    loader: 'expose-loader',
-                    options: '$',
-                }],
-            }, */
         ],
     },
-
-    watchOptions: {
-        poll: Mix.isPolling() ? 1700 : false,
-        aggregateTimeout: 600,
-        ignored: [ /^(?!.*resources)/ ]
-    },
-});
-
-/* mix.autoload({
-    jquery: ['$', 'jQuery', 'window.$', 'window.jQuery']
-}); */
+})
 
 // Asset Config
-
 let getFiles = function (dir) {
-    return fs.readdirSync(dir).filter(file => {
-        return fs.statSync(`${dir}/${file}`).isFile();
-    });
-};
+    return fs.readdirSync(dir).filter((file) => {
+        return fs.statSync(`${dir}/${file}`).isFile()
+    })
+}
 
 // Get all JS and output to individual files
 getFiles('resources/js').forEach(function (filepath) {
-    mix.js('resources/js/' + filepath, 'assets/js');
-});
+    mix.js('resources/js/' + filepath, 'assets/js')
+})
 
 // Do the rest
 mix.sass('resources/sass/styles.scss', 'assets/css')
     .extract()
     .sourceMaps()
-    .version();
+    .version()
