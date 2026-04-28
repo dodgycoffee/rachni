02847302d4e4caa12fd4d6e1280e89bb3241d166
Alex Brindley <alex@brindly.art>
2026-04-27 22:07:45 -0600

    tweak install

--- install.js
+++ install.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { copySync, readJsonSync, writeJsonSync, writeFileSync, removeSync } from 'fs-extra'
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { execa } from 'execa'
 import chalk from 'chalk'
+import fsExtra from 'fs-extra'
+const { copySync, readJsonSync, writeJsonSync, writeFileSync, removeSync } = fsExtra
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const src = resolve(__dirname)
